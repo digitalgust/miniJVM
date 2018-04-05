e29e7d9bf64035191872e5fe7c76e141d49c1bf6
digitalgust <digitalgust@163.com>
2018-04-05 12:15:33 +0800

    fix load class mechanism

---
+++ test/javalib_test/src/test/Foo3.java
@@ -0,0 +1,9 @@
+package test;
+
+class Foo3 {
+
+    public static void main(String args[]) {
+      Object o=null;
+      o.toString();
+    }
+}
--- test/jvm_macos/jvm_macos/invoke.c
+++ test/jvm_macos/jvm_macos/invoke.c
@@ -19,13 +19,16 @@ int call_jvm(char* app_path) {
     strcat(path,";");
     strcat(path,app_path);
     strcat(path,"/lib/luaj.jar");
+    strcat(path,";");
+    strcat(path,app_path);
+    strcat(path,"/lib/minijvm_test.jar");
     printf("classpath: %s\n",path);
     java_debug=0;
     ArrayList * java_para=arraylist_create(0);
-//    ret= execute_jvm(path, "test/Gears", java_para);
+    ret= execute_jvm(path, "test/Foo3", java_para);
 //    ret= execute_jvm(path, "test/GuiTest", java_para);
 //    ret= execute_jvm(path, "test/Light", java_para);
-    ret= execute_jvm(path, "Sample", java_para);
+//    ret= execute_jvm(path, "Sample", java_para);
     arraylist_destory(java_para);
     return ret;
 }
