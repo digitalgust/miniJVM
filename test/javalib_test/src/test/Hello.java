0546b47078678915b1d1a95068bdb851a4bbce14
digitalgust <digitalgust@163.com>
2018-08-13 12:39:42 +0800

    fix exception when jvm destory no close daemon thread

--- test/javalib_test/src/test/Hello.java
+++ test/javalib_test/src/test/Hello.java
@@ -2,13 +2,33 @@ package test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class Hello {
 
+    static Timer timer = new Timer(true);
+
+    static TimerTask task = new TimerTask() {
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(".");
+            } catch (Exception e) {
+            }
+        }
+    };
+
     public static void main(String[] args) {
-        Map<Long, String> map = new HashMap();
-        map.put(5L, "a");
-        String s=map.get(5L);
+
+        timer.schedule(task, 0, 1000);
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
+        }
     }
 
 }
