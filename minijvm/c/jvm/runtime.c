b2b18bb9c84eb9817812fadb95a53c3b8953ae1a
digitalgust <digitalgust@163.com>
2020-09-17 15:21:27 +0800

    fix native print stacktrack lineno

--- minijvm/c/jvm/runtime.c
+++ minijvm/c/jvm/runtime.c
@@ -160,7 +160,9 @@ s64 getInstructPointer(Runtime *runtime) {
 
 void getRuntimeStack(Runtime *runtime, Utf8String *ustr) {
     s32 i, imax;
-    utf8_append_c(ustr, "Exception threw :\n");
+    utf8_append_c(ustr, "Exception threw: ");
+    utf8_append(ustr, (runtime->stack->sp - 1)->ins->mb.clazz->name);
+    utf8_pushback(ustr, '\n');
     for (i = 0, imax = runtime->threadInfo->stacktrack->length; i < imax; i++) {
         utf8_append_c(ustr, "    at ");
         MethodInfo *method = arraylist_get_value(runtime->threadInfo->stacktrack, i);
@@ -171,7 +173,7 @@ void getRuntimeStack(Runtime *runtime, Utf8String *ustr) {
             utf8_append_c(ustr, "(");
             utf8_append(ustr, method->_this_class->source);
             utf8_append_c(ustr, ":");
-            s32 lineNo = method->converted_code ? getLineNumByIndex(method->converted_code, (s32) (intptr_t) arraylist_get_value(runtime->threadInfo->lineNo, i)) : -1;
+            s32 lineNo = method->converted_code ? (s32) (intptr_t) arraylist_get_value(runtime->threadInfo->lineNo, i) : -1;
             utf8_append_s64(ustr, lineNo, 10);
             utf8_append_c(ustr, ")");
         }
