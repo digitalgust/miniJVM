b8daf12ad25a9574a6434cedf4ac98c4f8882d9c
Gust <digitalgust@163.com>
2023-05-31 00:41:14 +0800

    update test

--- binary/win32/test.bat
+++ binary/win32/test.bat
@@ -1,4 +1,4 @@
-mini_jvm.exe -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/glfw_gui.jar org.mini.glfw.GlfwMain
+mini_jvm.exe -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/glfw_gui.jar;../libex/xgui.jar org.mini.glfw.GlfwMain
 
 @echo test
 mini_jvm.exe -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/minijvm_test.jar test.Foo3
