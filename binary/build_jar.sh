ae65f73e1f3c9ae07c5a4ecffc3b3745fa0f7f9d
Gust <digitalgust@163.com>
2023-12-19 14:47:47 +0800

    Update build_jar.sh

--- binary/build_jar.sh
+++ binary/build_jar.sh
@@ -9,36 +9,36 @@ JAR=jar
 # returns the JDK version.
 # 8 for 1.8.0_nn, 9 for 9-ea etc, and "no_java" for undetected
 jdk_version () {
-  result=""
-  java_cmd=""
-  if [[ -n $(type -p java) ]]
-  then
-    java_cmd=java
-  elif [[ (-x "$JAVA_HOME/bin/java") ]]
-  then
-    java_cmd="$JAVA_HOME/bin/java"
-  fi
-  IFS=$'\n'
-  # remove \r for Cygwin
-  lines=$("$java_cmd" -Xms32M -Xmx32M -version 2>&1 | tr '\r' '\n')
-  if [[ -z $java_cmd ]]
-  then
-    result=no_java
-  else
-    for line in $lines; do
-      if [[ (-z $result) && ($line = *"version \""*) ]]
-      then
-        ver=$(echo $line | sed -e 's/.*version "\(.*\)"\(.*\)/\1/; 1q')
-        # on macOS, sed doesn't support '?'
-        if [[ $ver = "1."* ]]
-        then
-          result=$(echo $ver | sed -e 's/1\.\([0-9]*\)\(.*\)/\1/; 1q')
-        else
-          result=$(echo $ver | sed -e 's/\([0-9]*\)\(.*\)/\1/; 1q')
-        fi
-      fi
-    done
-  fi
+  result="$(type -p java)"
+#  java_cmd=""
+#  if [[ -n $(type -p java) ]]
+#  then
+#    java_cmd=java
+#  elif [[ (-n "$JAVA_HOME") && (-x "$JAVA_HOME/bin/java") ]]
+#  then
+#    java_cmd="$JAVA_HOME/bin/java"
+#  fi
+#  IFS=$'\n'
+#  # remove \r for Cygwin
+#  lines=$("$java_cmd" -Xms32M -Xmx32M -version 2>&1 | tr '\r' '\n')
+#  if [[ -z $java_cmd ]]
+#  then
+#    result=no_java
+#  else
+#    for line in $lines; do
+#      if [[ (-z $result) && ($line = *"version \""*) ]]
+#      then
+#        ver=$(echo $line | sed -e 's/.*version "\(.*\)"\(.*\)/\1/; 1q')
+#        # on macOS, sed doesn't support '?'
+#        if [[ $ver = "1."* ]]
+#        then
+#          result=$(echo $ver | sed -e 's/1\.\([0-9]*\)\(.*\)/\1/; 1q')
+#        else
+#          result=$(echo $ver | sed -e 's/\([0-9]*\)\(.*\)/\1/; 1q')
+#        fi
+#      fi
+#    done
+#  fi
   echo "$result"
 }
 
