d48285535667aa6c43c72adfd882e76c19a9c004
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2024-01-14 14:15:05 +0100

    Fixed the Java part of ImageIO

--- minijvm/java/src/main/java/javax/imageio/ImageIO.java
+++ minijvm/java/src/main/java/javax/imageio/ImageIO.java
@@ -1,26 +1,28 @@
 package javax.imageio;
 
 import java.io.InputStream;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
 import java.awt.image.BufferedImage;
 
-public ImageIO {
+public class ImageIO {
     private static byte[] readAllBytes(InputStream inputStream) throws IOException {
         final int bufLen = 4 * 0x400; // 4KB
         byte[] buf = new byte[bufLen];
         int readLen;
         IOException exception = null;
 
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
-            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
-                while ((readLen = inputStream.read(buf, 0, bufLen)) != -1)
-                    outputStream.write(buf, 0, readLen);
+            while ((readLen = inputStream.read(buf, 0, bufLen)) != -1)
+                outputStream.write(buf, 0, readLen);
 
-                return outputStream.toByteArray();
-            }
+            return outputStream.toByteArray();
         } catch (IOException e) {
             exception = e;
             throw e;
         } finally {
+            outputStream.close();
             if (exception == null) inputStream.close();
             else try {
                 inputStream.close();
@@ -30,9 +32,9 @@ public ImageIO {
         }
     }
 
-    private static native byte[] readInternal(byte[] in, int[] size);
+    private static native int[] readInternal(byte[] in, int[] size);
 
-    public static BufferedImage read(InputStream is) {
+    public static BufferedImage read(InputStream is) throws IOException {
         byte[] bytes = readAllBytes(is);
         int[] size = { 0, 0 };
         int[] rgb = readInternal(bytes, size);
