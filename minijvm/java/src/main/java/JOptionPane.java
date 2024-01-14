381530b6b8949cddf3bd44728d4037a0a088a9e7
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2024-01-14 11:47:20 +0100

    Fix Swing

--- minijvm/java/src/main/java/JOptionPane.java
+++
@@ -1,5 +0,0 @@
-package javax.swing;
-
-public class JOptionPane {
-    void showMessageDialog(java.awt.Component c, Object o, String s) {}
-}
---
+++ minijvm/java/src/main/java/javax/swing/JOptionPane.java
@@ -0,0 +1,7 @@
+package javax.swing;
+
+public class JOptionPane {
+    void showMessageDialog(java.awt.Component c, Object o, String s, int i) {
+      System.out.println(s);
+    }
+}
