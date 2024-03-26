c9e34783871fdbae05ea467dbbb81a4ae04810ca
Gust <digitalgust@163.com>
2024-03-26 20:48:26 +0800

    fix audio issue

--- binary/mac_x64/test.sh
+++ binary/mac_x64/test.sh
@@ -1,10 +1,10 @@
 
-./mini_jvm -Xdebug -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/glfw_gui.jar:../libex/xgui.jar org.mini.glfw.GlfwMain
+$(dirname $0)/mini_jvm -Xdebug -bootclasspath $(dirname $0)/../lib/minijvm_rt.jar -cp $(dirname $0)/../libex/glfw_gui.jar:$(dirname $0)/../libex/xgui.jar org.mini.glfw.GlfwMain
 
-./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/minijvm_test.jar test.Foo3
+$(dirname $0)/mini_jvm -bootclasspath $(dirname $0)/../lib/minijvm_rt.jar -cp $(dirname $0)/../libex/minijvm_test.jar test.Foo3
 
 #./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/janino.jar:../libex/commons-compiler.jar #org.codehaus.janino.Compiler  ../res/BpDeepTest.java
 
-echo execute BpDeepTest
+#echo execute BpDeepTest
 #./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../res/ BpDeepTest
 #./mini_jvm -bootclasspath ../lib/minijvm_rt.jar -cp ../libex/luaj.jar Sample
