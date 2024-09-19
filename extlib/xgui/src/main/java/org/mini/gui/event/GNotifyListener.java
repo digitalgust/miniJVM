2013bf500e5e8178c9976a9eda4f2d908800535a
Gust <digitalgust@163.com>
2024-09-19 23:33:12 +0800

    Update GNotifyListener.java

--- extlib/xgui/src/main/java/org/mini/gui/event/GNotifyListener.java
+++ extlib/xgui/src/main/java/org/mini/gui/event/GNotifyListener.java
@@ -6,10 +6,11 @@
 package org.mini.gui.event;
 
 /**
- *
  * @author Gust
  */
 public interface GNotifyListener {
+    public static final String NOTIFY_KEY_DEVICE_TOKEN = "glfm.device.token";
+    public static final String NOTIFY_KEY_IOS_PURCHASE = "glfm.ios.purchase";
 
     void onNotify(String key, String val);
 }
