0a456d779753feb390cca10aae4ab29a3c70e32a
Gust <digitalgust@163.com>
2025-02-14 21:36:30 +0800

    Update jni_reflect.c

--- minijvm/c/jvm/jni_reflect.c
+++ minijvm/c/jvm/jni_reflect.c
@@ -1557,7 +1557,7 @@ s32 com_misc_Unsafe_compareAndSwapLong(Runtime *runtime, JClass *clazz) {
 #if __JVM_ARCH_64__
         s32 ret = (s32) ATOMIC_CAS((s64 *) src, oldv, newv);
 #else
-        s32 ret = __sync_bool_compare_and_swap((s64 *) src, oldv, newv);
+        s32 ret = ATOMIC_CAS((s64 *) src, oldv, newv);
 #endif
         push_int(runtime->stack, ret);
         return 0;
@@ -1585,7 +1585,7 @@ s32 com_misc_Unsafe_compareAndSwapObject(Runtime *runtime, JClass *clazz) {
         if (sizeof(__refer) == 8) {
             ret = ATOMIC_CAS((s64 *) src, (s64) (intptr_t) oldv, (s64) (intptr_t) newv);
         } else {
-            ret = __sync_bool_compare_and_swap((s32 *) src, (s32) (intptr_t) oldv, (s32) (intptr_t) newv);
+            ret = ATOMIC_CAS((s32 *) src, (s32) (intptr_t) oldv, (s32) (intptr_t) newv);
         }
         push_int(runtime->stack, ret);
         return 0;
