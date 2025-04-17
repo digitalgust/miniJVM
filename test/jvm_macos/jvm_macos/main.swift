46cb53c087bc08514c605580d51477e638ac1fab
digitalgust <digitalgust@163.com>
2025-04-17 20:52:33 +0800

    update macos test

--- test/jvm_macos/jvm_macos/main.swift
+++ test/jvm_macos/jvm_macos/main.swift
@@ -1,22 +1,24 @@
-//
-//  main.swift
-//  jvm_macos
-//
-//  Created by Gust on 2018/1/30.
-//  Copyright © 2018年 Gust. All rights reserved.
-//
-
 import Foundation
+import Darwin // 引入Darwin库以使用strdup和free函数
 
+// 假设这个函数已经在其他地方定义
+// void call_jvm(char *path);
 
-func execjvm()    {
-    
+func execjvm() {
     let mainBundle = Bundle.main.bundlePath
-    let app_path=mainBundle.cString(using: String.Encoding.utf8)
-    call_jvm(UnsafeMutablePointer<Int8>(mutating: app_path));
+    var appPath: UnsafeMutablePointer<CChar>? = nil
+    mainBundle.withCString { originalPath in
+        appPath = strdup(originalPath)
+    }
+    
+    if let validAppPath = appPath {
+        call_jvm(validAppPath)
+        // 使用完后释放内存
+        free(validAppPath)
+    } else {
+        print("Failed to convert the bundle path to a C string.")
+    }
 }
 
 print("Hello, World!")
-
 execjvm()
-
